474de04ab97c33461fb3f080a795e22be3a21c3f
ookbf0 <ookbf0@gmail.com>
2026-08-29 21:40:23 +0800

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,5 +1,3 @@
-export const DEFAULT_MODEL = "kimi-k3";
-
-export const MODELS = [
-  { id: "kimi-k3", label: "Kimi-K3", persona: 1 },
+window.APP_MODELS = [
+  { id: "kimi-k3", label: "Kimi-K3" },
 ];
